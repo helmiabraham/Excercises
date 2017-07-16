c2c8e1cebf2c4ceed6c326463398d1754165315b
helmiabraham <helmetofb2@gmail.com>
2017-07-16 15:17:21 +0530

    Add files via upload

---
+++ exp13.py
@@ -0,0 +1,10 @@
+months = ['jan','feb','mar','apr' ]
+months.append('may')
+months.append('july')
+months.insert(5,'jun')
+print months.index('may')
+months.remove('may')
+months.sort()
+months.reverse()
+print months
+
